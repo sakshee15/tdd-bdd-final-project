37c93ac9fc805692ad11a4af249d06b931c2dd61
Sakshee Agrawal <sakshee150503@gmail.com>
2024-10-06 12:32:17 -0400

    push code

--- tests/factories.py
+++ tests/factories.py
@@ -14,13 +14,14 @@
 
 # pylint: disable=too-few-public-methods
 
-"""
-Test Factory to make fake objects for testing
-"""
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
+"""
+Test Factory to make fake objects for testing
+"""
+## Add code to create Fake Products 
 
 class ProductFactory(factory.Factory):
     """Creates fake products for testing"""
@@ -31,4 +32,33 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+        ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
+
+
